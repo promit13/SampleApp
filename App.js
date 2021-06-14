975cea8d57c8ef08e3bee4353bc501b2794cfd80
Promit Rai <promitrai@Promits-MacBook-Pro.local>
2021-06-14 18:29:48 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {SafeAreaView, Text} from 'react-native';
+import {Home} from './src/screens/Home';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => Node = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <Home />
+    </SafeAreaView>
+  );
+};
+
+export default App;
